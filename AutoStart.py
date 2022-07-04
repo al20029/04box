8756ef84135d8a87f8b497b50723b2c877887396
AL86-HS <106137220+AL86-HS@users.noreply.github.com>
2022-07-05 05:12:08 +0900

    AutoStart.py

--- AutoStart.py
+++ AutoStart.py
@@ -1,1 +1,16 @@
-a
+from UI_main import UIMainProcess
+import schedule
+import datetime
+import time 
+
+UIMainProcess.Regular()
+def task():
+    print(datetime)
+    UIMainProcess.Regular()
+
+schedule.every(1).hours.do(task)
+# schedule.every(10).seconds.do(task)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
