2d3da324ec259c801ad4e380ad23e2084e56a8a4
太田 峻輔 <106137317+al20029@users.noreply.github.com>
2022-07-05 11:44:55 +0900

    計測終了画面完成11:44

--- Sato/DisplayFinishWindow.py
+++ Sato/DisplayFinishWindow.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
 from ToolTip import *
+from MainMeasurement import MainMeasurement
 
 class DisplayFinishWindow:
     def FinishWindow(CanConnectWiFiname,DataList):
@@ -40,12 +41,12 @@ class DisplayFinishWindow:
         Start = False
         # 画面作成
         frm = tkinter.Tk()
-        frm.geometry('800x500') #画面サイズ
-        frm.title()
+        frm.geometry('900x500') #画面サイズ
+        frm.title('計測終了画面')
 
         # 題名表示
         SystemName = tkinter.Label(text="計測終了", font=("MSゴシック", "30", "bold"))
-        SystemName.place(x=310, y=10)
+        SystemName.place(x=380, y=10)
         
         # グラフ表示
         fig = plt.Figure() #描画の用意
@@ -65,35 +66,78 @@ class DisplayFinishWindow:
       
         canvas = FigureCanvasTkAgg(fig, master=frm)
         canvas.draw()
-        canvas.get_tk_widget().place(x=10,y=62,width=500,height=380)
+        canvas.get_tk_widget().place(x=10,y=62,width=390,height=380)
 
-        #WiFiの名前と色をテキスト表示
-        str = ''
-        labelcount = 0
-        for Data in DataList:
-            labelcount += 1
-            str = ''
-            str += '   '
-            str += Data.WiFiname
-            XLabel = tkinter.Label(text = str,font=('MSゴシック','13'))
-            XLabel.place(x=30,y=445)
-            #canvas.create_line(20, 451, 27, 451, width = 2,fill = "Red" )
-            XLabel = tkinter.Label(text = "ー",font=('MSゴシック','17','bold'),fg="red")
-            XLabel.place(x=17,y=442)
+        # #WiFiの名前と色をテキスト表示
+        # temp = ''
+        # labelcount = 0
+        # for Data in DataList:
+        #     labelcount += 1
+        #     temp = ''
+        #     temp += '   '
+        #     temp += Data.WiFiname
+        #     XLabel = tkinter.Label(text = temp,font=('MSゴシック','13'))
+        #     XLabel.place(x=30,y=445)
+        #     #canvas.create_line(20, 451, 27, 451, width = 2,fill = "Red" )
+        #     XLabel = tkinter.Label(text = "ー",font=('MSゴシック','17','bold'),fg="red")
+        #     XLabel.place(x=17,y=442)
 
         #結果表
-        column = ('WiFi名', 'Speed', '安定性')
+        # ★バグ対応用の関数を追加
+        style = ttk.Style()
+        def fixed_map(option):
+            return [elm for elm in style.map('Treeview', query_opt=option) if
+            elm[:2] != ('!disabled', '!selected')]
+
+
+
+        column = ('WiFi名', 'Speed', 'stab0','stab1','stab2','stab3','stab4')
         tree = ttk.Treeview(frm,columns=column)
+
         tree.column('#0', width = 0, stretch = 'no')
         tree.column('WiFi名', anchor ='center', width = 110)
         tree.column('Speed', anchor = 'center', width = 60)
-        tree.column('安定性', anchor = 'center', width = 60) 
+        tree.column('stab0', anchor = 'center', width = 40)
+        tree.column('stab1', anchor = 'center', width = 60)
+        tree.column('stab2', anchor = 'center', width = 40)
+        tree.column('stab3', anchor = 'center', width = 60)
+        tree.column('stab4', anchor = 'center', width = 90) 
+
         tree.heading('WiFi名',text = 'WiFi名', anchor = 'center')
         tree.heading('Speed',text = 'Speed', anchor = 'center')
-        tree.heading('安定性',text = '安定性', anchor = 'center') 
-        for i in range(20):
-            tree.insert(parent='', index='end', iid = i, values=(DataList[0].WiFiname,i,0))
-        tree.place(x=530, y=80)     
+        tree.heading('stab0',text = 'メール', anchor = 'center')
+        tree.heading('stab1',text = 'ネット検索', anchor = 'center')
+        tree.heading('stab2',text = 'SNS', anchor = 'center')
+        tree.heading('stab3',text = '動画視聴', anchor = 'center')
+        tree.heading('stab4',text = 'オンラインゲーム', anchor = 'center')
+
+        
+        style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
+
+        tree.tag_configure("r", foreground='red')
+        tree.tag_configure("b", foreground='blue')
+        tree.tag_configure("g", foreground='green')
+        tree.tag_configure("y", foreground='yellow')
+        tree.tag_configure("m", foreground='magenta')
+        tree.tag_configure("c", foreground='cyan')
+        tree.tag_configure("k", foreground='black')
+
+        treecount = 0
+        for Data in DataList:
+            WiFiname = Data.WiFiname
+            avgspeed = round(MainMeasurement.AverageSpeedMeasurement(Data.ListInstantSpeed,len(Data.ListInstantSpeed)),1)
+            if len(Data.ListInstantSpeed) != 10:
+                stab =[-1,-1,-1,-1,-1]
+            else:
+                stab = MainMeasurement.StabilityCalculation(Data.ListInstantSpeed,10)
+            speed = str(avgspeed) +'Mbps'
+            tree.insert(parent='', index='end', iid = treecount, values=(WiFiname,speed,stab[0],stab[1],stab[2],stab[3],stab[4]),tags=Data.color)
+            treecount += 1
+        tree.place(x=420, y=80)     
+
+        #安定性の説明
+        notes = tkinter.Label(text = "※\'メール\'～\'オンラインゲーム\'は用途における安定性の目安\n  1～5の値を取り，値が大きいほど良い\n計測を途中で中止したときは-1の値を取る",font=('MSゴシック','13'))
+        notes.place(x=430,y=320)
 
         # Wi-Fi名プルタブ配置
         list = CanConnectWiFiname
--- Sato/test.py
+++ Sato/test.py
@@ -1,32 +1,4 @@
-# サンプルコード
-import tkinter as tk
-from tkinter import ttk
-
-# 列の識別名を指定
-column = ('ID', 'Name', 'Score')
-# メインウィンドウの生成
-root = tk.Tk()
-root.title('Score List')
-root.geometry('400x300')
-# Treeviewの生成
-tree = ttk.Treeview(root, columns=column)
-# 列の設定
-tree.column('#0',width=0, stretch='no')
-tree.column('ID', anchor='center', width=80)
-tree.column('Name',anchor='w', width=100)
-tree.column('Score', anchor='center', width=80)
-# 列の見出し設定
-tree.heading('#0',text='')
-tree.heading('ID', text='ID',anchor='center')
-tree.heading('Name', text='Name', anchor='w')
-tree.heading('Score',text='Score', anchor='center')
-# レコードの追加
-tree.insert(parent='', index='end', iid=0 ,values=(1, 'KAWASAKI',80))
-tree.insert(parent='', index='end', iid=1 ,values=(2,'SHIMIZU', 90))
-tree.insert(parent='', index='end', iid=2, values=(3,'TANAKA', 45))
-tree.insert(parent='', index='end', iid=3, values=(4,'OKABE', 60))
-tree.insert(parent='', index='end', iid=4, values=(5,'MIYAZAKI', 99))
-# ウィジェットの配置
-tree.pack(pady=10)
-
-root.mainloop()
+a = 0.11111
+c = str(round(a,3)) + 'mbps'
+print(c)
+print(type(c))
