75cf2068998818eec28ca9f6c77f4c3500f1dea9
AL86-HS <106137220+AL86-HS@users.noreply.github.com>
2022-07-05 14:54:51 +0900

    ssh.py

--- Sato/ssh.py
+++ Sato/ssh.py
@@ -1,8 +1,9 @@
 import paramiko
 
 client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.WarningPolicy())
-client.connect(hostname = '160.16.141.77',username='root',password='pracb2022',port = 22)
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname = '160.16.141.77',username='root',password='pracb2022',port = 50422)
 ssh = client.get_transport().open_session()
 stdin, stdout, stderr = client.exec_command("python3 hello.py")
+print(stdout.read())
 client.close()
