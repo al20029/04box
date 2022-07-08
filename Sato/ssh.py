a3f4e7951cff567d518638da20fb49537c892b28
AL86-HS <106137220+AL86-HS@users.noreply.github.com>
2022-07-08 21:42:26 +0900

    21:41

--- Sato/ssh.py
+++ Sato/ssh.py
@@ -15,10 +15,10 @@ class ssh():
         client.connect(hostname = '160.16.141.77',username='root',password='pracb2022',port = 50422)
         ssh = client.get_transport().open_session()
         argument = WiFiName + " " +  str(AverageSpeed) + " " +  str(Stability)
-        client.exec_command("python3 MainManagementWiFi.py"  + " 1 " + argument)
-        # stdin, stdout, stderr = client.exec_command("python3 MainManagementWiFi.py"  + " 1 " + argument)
+        # client.exec_command("python3 MainManagementWiFi.py"  + " 1 " + argument)
+        stdin, stdout, stderr = client.exec_command("python3 MainManagementWiFi.py"  + " 1 " + argument)
         # print(stderr.read().decode('utf-8'))
-        # print(stdout.read().decode('utf-8'))
+        print(stdout.read().decode('utf-8'))
         # print(stdout.read())
         client.close()
 
@@ -30,8 +30,14 @@ class ssh():
         ssh = client.get_transport().open_session()
         argument = WiFiName
         stdin, stdout, stderr = client.exec_command("python3 MainManagementWiFi.py" + " 2 " + argument)
-        print(stdout.read().decode('utf-8'))
+        # print(stdout.read().decode('utf-8'))
+        ######変更点######
+        ReturnValue = stdout.read().decode('UTF-8').split()
+        BestAverageSpeed = ReturnValue[0]
+        BestStability = ReturnValue[1]
         client.close()
+        return BestAverageSpeed, BestStability
+        ##################
 
     ## リアルタイムデータの取得
     def ParamikoGetReal(WiFiList):
@@ -44,7 +50,29 @@ class ssh():
         ssh = client.get_transport().open_session()
         argument = " " + str(len(WiFiList)) + " " + " ".join(WiFiList)
         stdin, stdout, stderr = client.exec_command("python3 MainManagementWiFi.py"+ " 3 " + argument)
-        print(stdout.read().decode('utf-8'))
+        # print(stdout.read().decode('utf-8'))
+        ReturnValue = stdout.read().decode('UTF-8').splitlines()
+        # ReturnValue = stdout.read().split('\n')
+
+        ######変更点######
+        print("GetReturn")
+        print(ReturnValue)
+        WiFiName = list()
+        AverageSpeed = list()
+        Stability = list()
+        print("Data:")
+        for s in ReturnValue:
+            # print(s)
+            Spl_s = s.split()
+            WiFiName.append(Spl_s[0])
+            AverageSpeed.append(Spl_s[1])
+            Stability.append(Spl_s[2])
+        print("GetList")
+        print(WiFiName)
+        print(AverageSpeed)
+        print(Stability)
         # print(stdout.read())
         client.close()
+        return WiFiName, AverageSpeed, Stability
+        ##################
     ###########################################
